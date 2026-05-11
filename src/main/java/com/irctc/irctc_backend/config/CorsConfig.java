c8d3730cbc020f6d599ff7e8b178e7943186c3ff
Gautam Dev <gautamdev066@gmail.com>
2026-05-11 16:53:08 +0530

    Add Netlify URL to CORS

--- src/main/java/com/irctc/irctc_backend/config/CorsConfig.java
+++ src/main/java/com/irctc/irctc_backend/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://loquacious-gecko-adb26f.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false)
